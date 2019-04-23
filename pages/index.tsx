6c18ccf72ffb5f7a5ba241e0339f728834ab882e
Alex Devero <deveroalex@gmail.com>
2019-04-23 09:55:53 +0200

    Add hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react'
-import Link from 'next/link'
+import Head from 'next/head'
+// import Link from 'next/link'
+
+// Import components
+import { Hero } from './../components/hero'
+
+// Import styles
+import './../styles/styles.scss'
+
 
 export default () => (
   <div>
-    Hello World.{' '}
-    <Link href='/about'>
+    <Head>
+      <title>Devevero Studio</title>
+
+      <meta charSet="utf-8" />
+
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700,900" rel="stylesheet" />
+
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900" rel="stylesheet" />
+    </Head>
+    {/* <Link href='/about'>
       <a>About</a>
-    </Link>
+    </Link> */}
+    <Hero />
   </div>
 )
