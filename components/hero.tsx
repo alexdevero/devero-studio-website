d1e24e23944b3bbe33070d02f89f89e1a45d96db
Alex Devero <deveroalex@gmail.com>
2019-04-30 15:48:55 +0200

    Add hero video with overlay

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,18 +3,29 @@ import * as React from 'react'
 export const Hero = () => {
   return(
     <div className="hero">
-      <div className="glitch-wrapper">
-        <h1 className="hero__title">
-          <span className="glitch" data-text="DEVERO">DEVERO</span>
-          <span className="glitch" data-text="STUDIO">STUDIO</span>
-        </h1>
+      <div className="container">
+        <div className="glitch-wrapper">
+          <h1 className="hero__title">
+            <span className="glitch" data-text="DEVERO">DEVERO</span>
+            <span className="glitch" data-text="STUDIO">STUDIO</span>
+          </h1>
+        </div>
+
+        <h2 className="hero__subtitle">We help startups, small businesses and individuals enter and thrive in the digital world.</h2>
+
+        <div className="divider divider--red" />
+
+        <button href="#whatWeDo" className="btn btn--red" data-text="WHAT WE DO">What we do</button>
       </div>
 
-      <h2 className="hero__subtitle">We help individuals, startups and small businesses enter and thrive in the digital world.</h2>
+      <div className="video-wrapper">
+        <div className="video-overlay"></div>
 
-      <div className="divider divider--red" />
-
-      <button className="btn btn--red">What we do</button>
+        <video autoPlay={false} loop={false} muted={true} poster="/static/images/working-space.jpg">
+          <source src="/static/videos/working-space.webm" />
+          <source src="/static/videos/working-space.mp4" />
+        </video>
+      </div>
     </div>
   )
 }
