f6bb1622dba1cc7adc2b3c196048ab50b4303718
Alex Devero <deveroalex@gmail.com>
2019-05-03 10:40:15 +0200

    Implement waypoints in contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import $ from 'jquery'
 import ReCAPTCHA from "react-google-recaptcha"
+import { Waypoint } from 'react-waypoint'
 
 export class Contact extends React.Component {
   state = {
@@ -16,6 +17,7 @@ export class Contact extends React.Component {
   inputEmail
   inputName
   inputMessage
+  sectionContact: HTMLDivElement | null
 
   handleInputChange = (event: any) => {
     if (event.target.value.length > 0 && event.target.name !== 'formEmail') {
@@ -108,65 +110,73 @@ export class Contact extends React.Component {
     }
   }
 
+  handleWaypointEnter = () => {
+    this.sectionContact!.classList.add('is-visible')
+  }
+
   render() {
     return(
       <section>
         <div className="container pt-5 pb-5">
-          <h1 className="h3 text--center">Let's connect</h1>
+          <Waypoint onEnter={this.handleWaypointEnter}>
+            <div ref={el => this.sectionContact = el}>
+              <h1 className="h3 text--center">Let's connect</h1>
 
-          <div className="divider divider--black divider--center"></div>
+              <div className="divider divider--black divider--center"></div>
 
-          <div className="row justify-content-center">
-            <div className="col-md-11 col-md-9 col-lg-7">
-              <form action="">
-                <div className="row">
-                  <div className="col-sm-6">
-                    <fieldset>
-                      <label htmlFor="formName">Full name</label>
+              <div className="row justify-content-center">
+                <div className="col-md-11 col-md-9 col-lg-7">
+                  <form action="">
+                    <div className="row">
+                      <div className="col-sm-6">
+                        <fieldset>
+                          <label htmlFor="formName">Full name</label>
 
-                      <input onChange={this.handleInputChange} id="formName" name="formName" type="text" required={true} ref={(inputName) => this.inputName = inputName} />
+                          <input onChange={this.handleInputChange} id="formName" name="formName" type="text" required={true} ref={(inputName) => this.inputName = inputName} />
+                        </fieldset>
+                      </div>
+
+                      <div className="col-sm-6 mt-3 mt-sm-0">
+                        <fieldset>
+                          <label htmlFor="formEmail">Email</label>
+
+                          <input onChange={this.handleInputChange} id="formEmail" name="formEmail" type="email" required={true} ref={(inputEmail) => this.inputEmail = inputEmail} />
+                        </fieldset>
+                      </div>
+                    </div>
+
+                    <fieldset className="mt-3 mb-3">
+                      <label htmlFor="formMessage">Message</label>
+
+                      <textarea onChange={this.handleInputChange} id="formMessage" name="formMessage" required={true} ref={(inputMessage) => this.inputMessage = inputMessage} />
                     </fieldset>
-                  </div>
 
-                  <div className="col-sm-6 mt-3 mt-sm-0">
-                    <fieldset>
-                      <label htmlFor="formEmail">Email</label>
-
-                      <input onChange={this.handleInputChange} id="formEmail" name="formEmail" type="email" required={true} ref={(inputEmail) => this.inputEmail = inputEmail} />
+                    <fieldset className="mb-4">
+                      <ReCAPTCHA
+                        sitekey="6Ldt6RgUAAAAAKtaxY2787y3S7uP5Wp9kzL0PMMg"
+                        size="normal"
+                        onChange={this.onCaptchaVerify}
+                      />
                     </fieldset>
-                  </div>
+
+                  {this.state.isErrorShown && (
+                    <fieldset className="mb-3">
+                        <p className="text--small"><strong>Please, make sure to fill all fields.</strong></p>
+                      </fieldset>
+                    )}
+
+                  {this.state.isFormSubmitted && (
+                    <fieldset className="mb-3">
+                        <p className="text--small"><strong>Your message is on the way. I will reply in three days.</strong></p>
+                      </fieldset>
+                    )}
+
+                    <button onClick={this.handleFormSubmit} className="btn btn--full-width btn--red" data-text="Send inquiry">Send inquiry</button>
+                  </form>
                 </div>
-
-                <fieldset className="mt-3 mb-3">
-                  <label htmlFor="formMessage">Message</label>
-
-                  <textarea onChange={this.handleInputChange} id="formMessage" name="formMessage" required={true} ref={(inputMessage) => this.inputMessage = inputMessage} />
-                </fieldset>
-
-                <fieldset className="mb-4">
-                  <ReCAPTCHA
-                    sitekey="6Ldt6RgUAAAAAKtaxY2787y3S7uP5Wp9kzL0PMMg"
-                    size="normal"
-                    onChange={this.onCaptchaVerify}
-                  />
-                </fieldset>
-
-              {this.state.isErrorShown && (
-                <fieldset className="mb-3">
-                    <p className="text--small"><strong>Please, make sure to fill all fields.</strong></p>
-                  </fieldset>
-                )}
-
-              {this.state.isFormSubmitted && (
-                <fieldset className="mb-3">
-                    <p className="text--small"><strong>Your message is on the way. I will reply in three days.</strong></p>
-                  </fieldset>
-                )}
-
-                <button onClick={this.handleFormSubmit} className="btn btn--full-width btn--red" data-text="Send inquiry">Send inquiry</button>
-              </form>
+              </div>
             </div>
-          </div>
+          </Waypoint>
         </div>
       </section>
     )
