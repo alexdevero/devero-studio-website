aae74008f58240acc0edac367da2e8e05d30f390
Alex Devero <deveroalex@gmail.com>
2019-05-17 12:00:14 +0200

    Add glitch to hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -12,11 +12,21 @@ export const Hero = () => {
           </h1>
         </div> */}
 
-        <h2 className="hero__title">Delivering digital perfection</h2>
+        <div className="glitch-wrapper d-md-none">
+          <h1 className="hero__title">
+            <span className="glitch" data-text="Delivering">Delivering</span>
+            <span className="glitch" data-text="digital">digital</span>
+            <span className="glitch" data-text="perfection">perfection</span>
+          </h1>
+        </div>
 
-        <h3 className="hero__text">We build top-class websites and mobile apps for those who want the best.</h3>
+        <div className="glitch-wrapper d-none d-md-block">
+          <h1 className="hero__title glitch" data-text="Delivering digital perfection">Delivering digital perfection</h1>
+        </div>
 
-        <AnchorLink className="btn btn--red" href="#whatWeDo" offset={() => 100} data-text="&darr; LEARN MORE" style={{ maxWidth: '240px', textDecoration: 'none' }}>&darr; LEARN MORE</AnchorLink>
+        <h3 className="hero__text">We build top-class websites and apps for those who want the best.</h3>
+
+        <AnchorLink className="hero__link" href="#whatWeDo" offset={() => 100}>&darr; Learn more</AnchorLink>
       </div>
     </div>
   )
