9b5ab8968f6ae1be2b364183b8a5eb1578c6f76d
Alex Devero <deveroalex@gmail.com>
2019-05-18 10:02:33 +0200

    Move Recent works to independent component

--- components/services.tsx
+++ components/services.tsx
@@ -1,178 +1,91 @@
 import * as React from 'react'
-import ImageZoom from 'react-medium-image-zoom'
 import { Waypoint } from 'react-waypoint'
 
 export const Services = () => {
-  let sectionConcept: HTMLDivElement | null
-  let sectionDevelopment: HTMLDivElement | null
+  let sectionServices: HTMLDivElement | null
+  let sectionDesign: HTMLDivElement | null
+  let sectionEngineering: HTMLDivElement | null
   let sectionMaintenance: HTMLDivElement | null
 
   const handleWaypointEnter = (section: string) => {
     switch (section) {
-      case 'design':
-        sectionConcept!.classList.add('fade-in-up')
+      case 'services':
+        sectionServices!.classList.add('fade-in-up')
         break
-      case 'development':
-        sectionDevelopment!.classList.add('fade-in-up')
+      case 'design':
+        sectionDesign!.classList.add('fade-in-up')
+        break
+      case 'engineering':
+        sectionEngineering!.classList.add('fade-in-up')
         break
       case 'maintenance':
         sectionMaintenance!.classList.add('fade-in-up')
         break
-
     }
   }
 
   return(
     <section id="whatWeDo">
       <div className="container pt-5 pb-5">
-        <Waypoint onEnter={() => handleWaypointEnter('design')} topOffset="-40%">
-          <div className="services__row services__row--one row animated" ref={el => sectionConcept = el}>
-            <div className="col-md-6 col-lg-5">
-              <h2 className="h3 heading--small text--green">Concept &amp; Design</h2>
-
-              <div className="divider divider--gray divider--slim" />
-
-              <p>Our team of exceptional designers will create design concept, and later prototype, for your websites or web app based on your requirements. Next, we will review it with you, and gradually, polish it to perfection.</p>
-
-              <ul className="list--unstyled mt-3">
-                <li>&bull; User Experience Design</li>
-                <li>&bull; Interaction Design</li>
-                <li>&bull; Visual Design</li>
-                <li>&bull; Design QA</li>
-              </ul>
-            </div>
-
-            <div className="col-md-6 services__screenshot-wrapper">
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-12.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-12.jpg'),
-                  alt: ''
-                }}
-              />
-
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-11.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-11.jpg'),
-                  alt: ''
-                }}
-              />
-
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-06.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-06.jpg'),
-                  alt: ''
-                }}
-              />
+        <Waypoint onEnter={() => handleWaypointEnter('services')} topOffset="-40%">
+          <div className="services__row row">
+            <div className="col-md-6 col-lg-5 services__col-text animated" data-number="2" ref={el => sectionServices = el}>
+              <h3 className="h4">Services</h3>
             </div>
           </div>
         </Waypoint>
 
-        <Waypoint onEnter={() => handleWaypointEnter('development')} topOffset="-40%">
-          <div className="services__row services__row--two row mt-5 animated" ref={el => sectionDevelopment = el}>
-            <div className="col-md-6 services__screenshot-wrapper">
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-07.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-07.jpg'),
-                  alt: ''
-                }}
-              />
+        <div className="services__row services__row--two row mt-1">
+          <Waypoint onEnter={() => handleWaypointEnter('design')} topOffset="-40%">
+            <div className="col-md-6 services__col-text animated" ref={el => sectionDesign = el}>
+              <h2 className="h3 heading--small">Design</h2>
 
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-04.png'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-04.png'),
-                  alt: ''
-                }}
-              />
+              <div className="divider divider--red divider--slim" />
 
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-05.png'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-05.png'),
-                  alt: ''
-                }}
-              />
+              {/* <p>Our team of exceptional designers will create design concept, and later prototype, for your websites or web app based on your requirements. Next, we will review it with you, and gradually, polish it to perfection.</p> */}
 
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-08.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-08.jpg'),
-                  alt: ''
-                }}
-              />
+              <p>Balancing creativity and imagination with functionality to deliver unforgettable digital experiences that enhances the vision you have for your project and your brand.</p>
 
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-15.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-15.jpg'),
-                  alt: ''
-                }}
-              />
+              <ul className="list--unstyled mt-3">
+                <li>&bull; Identity Design</li>
+                <li>&bull; Interaction Design</li>
+                <li>&bull; User Experience Design</li>
+                <li>&bull; Visual Design</li>
+              </ul>
             </div>
+          </Waypoint>
 
-            <div className="col-md-6">
-              <h2 className="h3 heading--small text--green">Code &amp; Development</h2>
+          <Waypoint onEnter={() => handleWaypointEnter('engineering')} topOffset="-40%">
+            <div className="col-md-6 services__col-text mt-5 mt-md-0 animated" ref={el => sectionEngineering = el}>
+              <h2 className="h3 heading--small">Engineering</h2>
 
-              <div className="divider divider--center divider--gray divider--slim" />
+              <div className="divider divider--red divider--slim" />
 
-              <p>When you are pleased with design prototype, our team of rock star developers will get to work on it. They will take the design prototype and transform it into clean, optimized and ready-to-use website, or app.</p>
+              {/* <p>When you are pleased with design prototype, our team of rock star developers will get to work on it. They will take the design prototype and transform it into clean, optimized and ready-to-use website, or app.</p> */}
+
+              <p>Pushing the limits and boundaries of digital development, whilst producing functional unforgettable experiences that work for our partners and their users.</p>
 
               <ul className="list--unstyled mt-3">
                 <li>&bull; iOS &amp; Android (React Native)</li>
                 <li>&bull; Frontend (React)</li>
                 <li>&bull; Backend (Node.js)</li>
                 <li>&bull; Machine Learning</li>
-                <li>&bull; AR/VR</li>
-                <li>&bull; QA Testing</li>
+                <li>&bull; AR/VR/MR</li>
+                {/* <li>&bull; QA Testing</li> */}
               </ul>
             </div>
-          </div>
-        </Waypoint>
+          </Waypoint>
 
-        <Waypoint onEnter={() => handleWaypointEnter('maintenance')} topOffset="-40%">
-          <div className="services__row services__row--three row mt-5 animated" ref={el => sectionMaintenance = el}>
-            <div className="col-md-6">
-              <h2 className="h3 heading--small text--green">Shipping &amp; Maintenance</h2>
 
-              <div className="divider divider--gray divider--slim" />
+          <Waypoint onEnter={() => handleWaypointEnter('maintenance')} topOffset="-40%">
+            <div className="col-md-6 services__col-text mt-5 animated" ref={el => sectionMaintenance = el}>
+              <h2 className="h3 heading--small">Maintenance</h2>
 
-              <p>When your website is ready we will deploy it, put put it online. In case of an app, we will help you put it in stores. After this, we can take care of it for you if you want, maintaining, fixing and improving it.</p>
+              <div className="divider divider--red divider--slim" />
+
+              {/* <p>When your website is ready we will deploy it, put put it online. In case of an app, we will help you put it in stores. After this, we can take care of it for you if you want, maintaining, fixing and improving it.</p> */}
+
+              <p>Continually updating, improving and maintaining your website or app to ensure you stay on the cutting-edge in today's constantly changing and evolving world.</p>
 
               <ul className="list--unstyled mt-3">
                 <li>&bull; Reviews &amp; Reports</li>
@@ -180,46 +93,8 @@ export const Services = () => {
                 <li>&bull; Updates &amp; fixes</li>
               </ul>
             </div>
-
-            <div className="col-md-6 services__screenshot-wrapper">
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-09.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-09.jpg'),
-                  alt: ''
-                }}
-              />
-
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-01.png'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-01.png'),
-                  alt: ''
-                }}
-              />
-
-              <ImageZoom
-                image={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-14.jpg'),
-                  alt: '',
-                  className: 'services__screenshot',
-                }}
-                zoomImage={{
-                  src: require('./../static/images/screenshots-services/services-screenshot-14.jpg'),
-                  alt: ''
-                }}
-              />
-            </div>
-          </div>
-        </Waypoint>
+          </Waypoint>
+        </div>
       </div>
     </section>
   )
---
+++ components/works.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react'
+import ImageZoom from 'react-medium-image-zoom'
+import { Waypoint } from 'react-waypoint'
+
+export const Works = () => {
+  let sectionRecentWorks: HTMLDivElement | null
+
+  const handleWaypointEnter = (section: string) => {
+    switch (section) {
+      case 'recentWorks':
+        sectionRecentWorks!.classList.add('fade-in-up')
+        break
+    }
+  }
+
+  return(
+    <section>
+      <div className="container pt-5 pb-5">
+        <div className="services__row services__row--one row">
+          <Waypoint onEnter={() => handleWaypointEnter('recentWorks')} topOffset="-40%">
+            <div className="col-md-6 col-lg-5 services__col-text animated" data-number="1" ref={el => sectionRecentWorks = el}>
+              <h2 className="h4">Recent works</h2>
+
+              <div className="divider divider--red divider--slim" />
+
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+          </Waypoint>
+
+          {/* <div className="col-md-6 services__screenshot-wrapper">
+            <ImageZoom
+              image={{
+                src: require('./../static/images/screenshots-services/services-screenshot-12.jpg'),
+                alt: '',
+                className: 'services__screenshot',
+              }}
+              zoomImage={{
+                src: require('./../static/images/screenshots-services/services-screenshot-12.jpg'),
+                alt: ''
+              }}
+            />
+
+            <ImageZoom
+              image={{
+                src: require('./../static/images/screenshots-services/services-screenshot-11.jpg'),
+                alt: '',
+                className: 'services__screenshot',
+              }}
+              zoomImage={{
+                src: require('./../static/images/screenshots-services/services-screenshot-11.jpg'),
+                alt: ''
+              }}
+            />
+
+            <ImageZoom
+              image={{
+                src: require('./../static/images/screenshots-services/services-screenshot-06.jpg'),
+                alt: '',
+                className: 'services__screenshot',
+              }}
+              zoomImage={{
+                src: require('./../static/images/screenshots-services/services-screenshot-06.jpg'),
+                alt: ''
+              }}
+            />
+          </div> */}
+        </div>
+      </div>
+    </section>
+  )
+}
