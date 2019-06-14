6d1d7bff65141192eeb1dc18b5593b2b3e16d975
Alex Devero <deveroalex@gmail.com>
2019-06-14 10:28:41 +0200

    Restyle footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,64 +2,76 @@ import * as React from 'react'
 
 export const Footer = () => {
   return(
-    <footer className="pt-5 pb-5 text--center">
-      <ul className="list--inline list--unstyled">
-        <li className="list--inline-item">
-          <a href="https://twitter.com/alexdevero" target="_blank">
-            <span className="fab fa-twitter"></span>
-          </a>
-        </li>
+    <footer className="pt-5 pb-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <a className="link--unstyled" href="mailto:studio@devero.com">studio@devero.com</a>
 
-        <li className="list--inline-item">
-          <a href="https://github.com/alexdevero" target="_blank">
-            <span className="fab fa-github"></span>
-          </a>
-        </li>
+            <ul className="list--inline list--unstyled mt-3">
+              <li className="list--inline-item">
+                <a href="https://twitter.com/alexdevero" target="_blank">
+                  <span className="fab fa-twitter"></span>
+                </a>
+              </li>
 
-        {/* <li className="list--inline-item">
-          <a href="https://codesandbox.io/u/alexdevero" target="_blank">
-            <span className="fab fa-sandbox"></span>
-          </a>
-        </li> */}
+              <li className="list--inline-item">
+                <a href="https://github.com/alexdevero" target="_blank">
+                  <span className="fab fa-github"></span>
+                </a>
+              </li>
 
-        <li className="list--inline-item">
-          <a href="https://codepen.io/alexdevero/" target="_blank">
-            <span className="fab fa-codepen"></span>
-          </a>
-        </li>
+              {/* <li className="list--inline-item">
+                <a href="https://codesandbox.io/u/alexdevero" target="_blank">
+                  <span className="fab fa-sandbox"></span>
+                </a>
+              </li> */}
 
-        <li className="list--inline-item">
-          <a href="https://stackoverflow.com/story/alexdevero" target="_blank">
-            <span className="fab fa-stack-overflow"></span>
-          </a>
-        </li>
+              <li className="list--inline-item">
+                <a href="https://codepen.io/alexdevero/" target="_blank">
+                  <span className="fab fa-codepen"></span>
+                </a>
+              </li>
 
-        <li className="list--inline-item">
-          <a href="https://dribbble.com/alexdevero" target="_blank">
-            <span className="fab fa-dribbble"></span>
-          </a>
-        </li>
+              <li className="list--inline-item">
+                <a href="https://stackoverflow.com/story/alexdevero" target="_blank">
+                  <span className="fab fa-stack-overflow"></span>
+                </a>
+              </li>
 
-        {/* <li className="list--inline-item">
-          <a href="https://www.behance.net/alexdevero" target="_blank">
-            <span className="fab fa-behance"></span>
-          </a>
-        </li> */}
+              <li className="list--inline-item">
+                <a href="https://dribbble.com/alexdevero" target="_blank">
+                  <span className="fab fa-dribbble"></span>
+                </a>
+              </li>
 
-        {/* <li className="list--inline-item">
-          <a href="https://www.facebook.com/deveroalex" target="_blank">
-            <span className="fab fa-facebook"></span>
-          </a>
-        </li> */}
+              {/* <li className="list--inline-item">
+                <a href="https://www.behance.net/alexdevero" target="_blank">
+                  <span className="fab fa-behance"></span>
+                </a>
+              </li> */}
 
-        <li className="list--inline-item">
-          <a href="https://www.instagram.com/alexdevero/" target="_blank">
-            <span className="fab fa-instagram"></span>
-          </a>
-        </li>
-      </ul>
+              {/* <li className="list--inline-item">
+                <a href="https://www.facebook.com/deveroalex" target="_blank">
+                  <span className="fab fa-facebook"></span>
+                </a>
+              </li> */}
 
-      <p className="footer__copy">&copy;{new Date().getFullYear()} Devero Studio. All rights reserved.</p>
+              <li className="list--inline-item">
+                <a href="https://www.instagram.com/alexdevero/" target="_blank">
+                  <span className="fab fa-instagram"></span>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-md-6">
+            <p className="footer__copy text--right">&copy;{new Date().getFullYear()} Devero Studio.</p>
+
+            <p className="footer__copy text--right mt-2">All rights reserved</p>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
