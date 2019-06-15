c0211c794348e799d082422fc7852fb03d9009af
Alex Devero <deveroalex@gmail.com>
2019-06-15 10:47:58 +0200

    Roboto Roboto as body typeface

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,13 +41,13 @@ export default class extends React.Component {
 
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-          <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900" rel="stylesheet" />
+          {/* <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900" rel="stylesheet" /> */}
 
           <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap&subset=latin-ext" rel="stylesheet" />
 
           <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap&subset=latin-ext" rel="stylesheet" />
 
-          <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap&subset=latin-ext" rel="stylesheet" />
+          {/* <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap&subset=latin-ext" rel="stylesheet" /> */}
 
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
         </Head>
