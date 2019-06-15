122c0f4416a3c36d186e115922d6c426c8e4fc72
Alex Devero <deveroalex@gmail.com>
2019-06-15 10:44:05 +0200

    Add projects listing to Works

--- components/works.tsx
+++ components/works.tsx
@@ -8,6 +8,8 @@ export const Works = () => {
 
   const [activeTab, handleChangeActiveTab] = React.useState('one')
 
+  const [isProjectsListVisible, handleOpenProjectsList] = React.useState(false)
+
   const handleWaypointEnter = (section: string) => {
     switch (section) {
       case 'recentWorks':
@@ -114,53 +116,109 @@ export const Works = () => {
     <section className="pb-5">
       <div className="container pt-5 pb-5">
         <Waypoint onEnter={() => handleWaypointEnter('recentWorks')} topOffset="-40%">
-          <div className="services__row services__row--one row animated" ref={el => sectionRecentWorks = el}>
-            <div className="col-md-6 services__col-text" data-number="1">
-              <h2 className="h4">Recent works</h2>
+          <div>
+            <div className="services__row services__row--one row animated" ref={el => sectionRecentWorks = el}>
+              <div className="col-md-6 services__col-text" data-number="1">
+                <h2 className="h4">Recent works</h2>
 
-              <div className="divider divider--red divider--slim" />
+                <div className="divider divider--red divider--slim" />
 
-              <ul className="list--unstyled">
-                <li className="mb-2">
-                  <span className="link--black-red" onClick={() => handleChangeActiveTab('one')}><small className="text--small">No.00:</small> <strong>Tesla</strong></span>
-                </li>
+                <ul className="list--unstyled">
+                  <li className="mb-2">
+                    <span className="link--black-red" onClick={() => handleChangeActiveTab('one')}><small className="text--small">No.00:</small> <strong>Tesla</strong></span>
+                  </li>
 
-                <li className="mb-2">
-                  <span className="link--black-red" onClick={() => handleChangeActiveTab('two')}><small className="text--small">No.01:</small> <strong>Harley-Davidson</strong></span>
-                </li>
+                  <li className="mb-2">
+                    <span className="link--black-red" onClick={() => handleChangeActiveTab('two')}><small className="text--small">No.01:</small> <strong>Harley-Davidson</strong></span>
+                  </li>
 
-                <li className="mb-2">
-                  <span className="link--black-red" onClick={() => handleChangeActiveTab('three')}><small className="text--small">No.02:</small> <strong>Ceska Whisky</strong></span>
-                </li>
+                  <li className="mb-2">
+                    <span className="link--black-red" onClick={() => handleChangeActiveTab('three')}><small className="text--small">No.02:</small> <strong>Ceska Whisky</strong></span>
+                  </li>
 
-                <li className="mb-2">
-                  <span className="link--black-red" onClick={() => handleChangeActiveTab('four')}><small className="text--small">No.03:</small> <strong>Sumava</strong></span>
-                </li>
+                  <li className="mb-2">
+                    <span className="link--black-red" onClick={() => handleChangeActiveTab('four')}><small className="text--small">No.03:</small> <strong>Sumava</strong></span>
+                  </li>
 
-                {/* <li className="mb-2">
-                  <span className="link--black-red" onClick={() => handleChangeActiveTab('four')}><small className="text--small">No.03:</small> <strong>Foo</strong></span>
-                </li> */}
+                  {/* <li className="mb-2">
+                    <span className="link--black-red" onClick={() => handleChangeActiveTab('four')}><small className="text--small">No.03:</small> <strong>Foo</strong></span>
+                  </li> */}
 
-                {/* <li className="mb-2">
-                  <span className="link--black-red" onClick={() => handleChangeActiveTab('five')}><small className="text--small">No.04:</small> <strong>Foo</strong></span>
-                </li> */}
+                  {/* <li className="mb-2">
+                    <span className="link--black-red" onClick={() => handleChangeActiveTab('five')}><small className="text--small">No.04:</small> <strong>Foo</strong></span>
+                  </li> */}
 
-                {/* <li className="mb-2">
-                  <span className="link--black-red" onClick={() => handleChangeActiveTab('six')}><small className="text--small">No.05:</small> <strong>Foo</strong></span>
-                </li> */}
+                  {/* <li className="mb-2">
+                    <span className="link--black-red" onClick={() => handleChangeActiveTab('six')}><small className="text--small">No.05:</small> <strong>Foo</strong></span>
+                  </li> */}
 
-                <li className="d-none">
-                  <Link href="portfolio"><a className="link--unstyled link--black-red"><strong>Show full portfolio &rarr;</strong></a></Link>
-                </li>
-              </ul>
+                  <li className="d-none">
+                    <Link href="portfolio"><a className="link--unstyled link--black-red"><strong>Show full portfolio &rarr;</strong></a></Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-md-6">
+                <div className="works__wrapper">
+                  {generateCaseStudyThumbnail()}
+
+                  {/* <Link href={generateCaseStudyBtnLink()}><a className="works__btn btn btn--red link--unstyled" data-text="See case study &rarr;">See case study &rarr;</a></Link> */}
+                </div>
+              </div>
             </div>
 
-            <div className="col-md-6">
-              <div className="works__wrapper">
-                {generateCaseStudyThumbnail()}
+            <div className="works__projects mt-7">
+              {isProjectsListVisible && <div className="row mb-5">
+                <div className="col-sm-2 col-md-4">
+                  <h2 className="h5">Web design</h2>
 
-                {/* <Link href={generateCaseStudyBtnLink()}><a className="works__btn btn btn--red link--unstyled" data-text="See case study &rarr;">See case study &rarr;</a></Link> */}
-              </div>
+                  <ul className="list--unstyled">
+                    <li>Tesla Motors</li>
+                    <li>Harley Davidson</li>
+                    <li>Česká Whisky</li>
+                    <li>Slavnosti Růžového Vína</li>
+                    <li>Slavnosti Růžového Vína</li>
+                    <li>Posli</li>
+                  </ul>
+                </div>
+
+                <div className="col-sm-2 col-md-4">
+                  <h2 className="h5 mt-4">Web development</h2>
+
+                  <ul className="list--unstyled">
+                    <li>Supernova</li>
+                    <li>Čerstvá Sváča</li>
+                    <li>Trinity</li>
+                    <li>Material</li>
+                    <li>Maelstrom</li>
+                    <li>Sumava</li>
+                  </ul>
+                </div>
+
+                <div className="col-sm-2 col-md-4">
+                  {/* <h2 className="h5">App design</h2>
+
+                  <ul className="list--unstyled">
+                    <li></li>
+                  </ul> */}
+
+                  <h2 className="h5">App development</h2>
+
+                  <ul className="list--unstyled">
+                    <li>Supernova Cloud</li>
+                    <li>StartMonday</li>
+                    <li>Účetnictví Online</li>
+                  </ul>
+
+                  <h2 className="h5">Consulting</h2>
+
+                  <ul className="list--unstyled">
+                    <li>DámeJídlo</li>
+                  </ul>
+                </div>
+              </div>}
+
+              <button className="works__projects-btn btn btn--black" onClick={() => handleOpenProjectsList(!isProjectsListVisible)} data-text={`${isProjectsListVisible ? 'Hide' : 'View'} all projects`}>{isProjectsListVisible ? 'Hide' : 'View'} all projects</button>
             </div>
           </div>
         </Waypoint>
