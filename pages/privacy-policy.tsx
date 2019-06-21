8d8f1fc4499029afd4ee68f6d995517b19ef8a5a
Alex Devero <deveroalex@gmail.com>
2019-06-21 14:45:30 +0200

    Update layout of Terms & Cond. & Privacy pages

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,47 +1,76 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+import { Footer } from './../components/footer'
+
+// Import styles
+import './../styles/styles.scss'
 
 export default () => (<div>
   <Head>
     <title>Privacy Policy | Devero Studio</title>
+
+    <meta charSet="utf-8" />
+
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap&subset=latin-ext" rel="stylesheet" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap&subset=latin-ext" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
   </Head>
 
-  <div>
-    <h1>Privacy Policy</h1>
+  <div className="container">
 
-    <h2 className="h2">Our commitment to privacy</h2>
+    <div className="row justify-content-center pt-5 pb-7">
+      <div className="col-md-10 col-lg-8">
+        <div className="text--center">
+          <h1 className="h1 mb-4">Privacy Policy</h1>
 
-    <p>Your privacy is important to us. To better protect your privacy we provide this notice explaining our information practices and the choices you can make about the way your information is collected and used. To make this notice easy to find, we make it available on every pages of our website.</p>
+          <Link href="/">
+            <a className="link--black-red text--small">&lt; back home</a>
+          </Link>
+        </div>
 
-    <h2 className="h2">The information we collect</h2>
+        <h2 className="h2 mt-5">Our commitment to privacy</h2>
 
-    <p>This notice applies to all information collected or submitted on the Devero Studio's website or via phone with a Devero Studio representative. The types of information collected are personal, billing and project related. We might also collect information from you regarding third parties in relation to you (i.e. hosting company etc.).</p>
+        <p className="mt-3">Your privacy is important to us. To better protect your privacy we provide this notice explaining our information practices and the choices you can make about the way your information is collected and used. To make this notice easy to find, we make it available on every pages of our website.</p>
 
-    <h2 className="h2">The way we use information</h2>
+        <h2 className="h2 mt-5">The information we collect</h2>
 
-    <p>We use the information you provide about yourself to provide services. We do not share this information with outside parties except to the extent necessary to complete your request for our services. Devero Studio reserves the right to reveal this information pursuant to a valid subpoena or court order.</p>
+        <p className="mt-3">This notice applies to all information collected or submitted on the Devero Studio's website or via phone with a Devero Studio representative. The types of information collected are personal, billing and project related. We might also collect information from you regarding third parties in relation to you (i.e. hosting company etc.).</p>
 
-    <p>We use return email addresses to answer the email we receive for tech support, customer service, tell-a-friend, email updates. Such addresses are not used for any other purpose and are not shared with outside parties.</p>
+        <h2 className="h2 mt-5">The way we use information</h2>
 
-    <p>Finally, we never use or share the personally identifiable information provided to us online, via phone, or in person, in ways unrelated to the ones described above without also providing you an opportunity to opt-out or otherwise prohibit such unrelated uses.</p>
+        <p className="mt-3">We use the information you provide about yourself to provide services. We do not share this information with outside parties except to the extent necessary to complete your request for our services. Devero Studio reserves the right to reveal this information pursuant to a valid subpoena or court order.</p>
 
-    <h2 className="h2">Our commitment to data security</h2>
+        <p className="mt-3">We use return email addresses to answer the email we receive for tech support, customer service, tell-a-friend, email updates. Such addresses are not used for any other purpose and are not shared with outside parties.</p>
 
-    <p>To prevent unauthorized access, maintain data accuracy, and ensure the correct use of information, we have put in place appropriate physical, electronic, and managerial procedures to safeguard and secure the information we collect online and offline.</p>
+        <p className="mt-3">Finally, we never use or share the personally identifiable information provided to us online, via phone, or in person, in ways unrelated to the ones described above without also providing you an opportunity to opt-out or otherwise prohibit such unrelated uses.</p>
 
-    <p>All online credit card transactions are performed only on secure servers through a secure SSL connection and comply with the latest standards in the industry. All billing information, including credit cards information, are deleted upon termination of the project with the client.</p>
+        <h2 className="h2 mt-5">Our commitment to data security</h2>
 
-    <h2 className="h2">Third-party websites</h2>
+        <p className="mt-3">To prevent unauthorized access, maintain data accuracy, and ensure the correct use of information, we have put in place appropriate physical, electronic, and managerial procedures to safeguard and secure the information we collect online and offline.</p>
 
-    <p>This website may provide links to third-party websites. Any such links are provided solely as a convenience to you. Devero Studio has no control over these websites or their content and does not assume any responsibility or liability for these websites.Devero Studio does not endorse or make any representations about these websites, or any information, materials, or products found thereon. If you access any of the third-party websites linked on this Web site, you do so at your own risk.</p>
+        <p className="mt-3">All online credit card transactions are performed only on secure servers through a secure SSL connection and comply with the latest standards in the industry. All billing information, including credit cards information, are deleted upon termination of the project with the client.</p>
 
-    <h2 className="h2">How you can access or correct your information</h2>
+        <h2 className="h2 mt-5">Third-party websites</h2>
 
-    <p>At any time, you may later review or update the personal information we have collected online from you by contacting us at: studio@devero.io</p>
+        <p className="mt-3">This website may provide links to third-party websites. Any such links are provided solely as a convenience to you. Devero Studio has no control over these websites or their content and does not assume any responsibility or liability for these websites.Devero Studio does not endorse or make any representations about these websites, or any information, materials, or products found thereon. If you access any of the third-party websites linked on this Web site, you do so at your own risk.</p>
 
-    <p>To protect your privacy and security, we will also take reasonable steps to verify your identity before granting access or making corrections.</p>
+        <h2 className="h2 mt-5">How you can access or correct your information</h2>
 
-    <h2 className="h2">How to contact us</h2>
+        <p className="mt-3">At any time, you may later review or update the personal information we have collected online from you by contacting us at: studio@devero.io</p>
 
-    <p>Should you have other questions or concerns about these privacy policies, please send us an email at studio@devero.io</p>
+        <p className="mt-3">To protect your privacy and security, we will also take reasonable steps to verify your identity before granting access or making corrections.</p>
+
+        <h2 className="h2 mt-5">How to contact us</h2>
+
+        <p className="mt-3">Should you have other questions or concerns about these privacy policies, please send us an email at studio@devero.io</p>
+      </div>
+    </div>
   </div>
+
+  <Footer />
 </div>)
--- pages/terms.tsx
+++ pages/terms.tsx
@@ -1,37 +1,65 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+import { Footer } from './../components/footer'
+
+// Import styles
+import './../styles/styles.scss'
 
 export default () => (<div>
   <Head>
     <title>Terms and Conditions | Devero Studio</title>
+
+    <meta charSet="utf-8" />
+
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap&subset=latin-ext" rel="stylesheet" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap&subset=latin-ext" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
   </Head>
 
-  <div>
-    <h1>Terms and Conditions</h1>
+  <div className="container">
+    <div className="row justify-content-center pt-5 pb-7">
+      <div className="col-md-10 col-lg-8">
+        <div className="text--center">
+          <h1 className="h1 mb-4">Terms and Conditions</h1>
 
-    <p>1. Devero Studio starts working on a project upon receipt of a down payment. Unless otherwise specified in writing, the client is charged the remaining balance upon completion of the project. Websites are made live upon completion of the project, when all payments are cleared. If applicable, hosting and domain name registration fees must be paid every month.</p>
+          <Link href="/">
+            <a className="link--black-red text--small">&lt; back home</a>
+          </Link>
+        </div>
 
-    <p>2. Additional services come at additional fees, on a fee for service basis or on an hourly rate. If clients require additional services, the correspondent fee is added to their invoice and they are charged accordingly.</p>
+        <p className="mt-3">1. Devero Studio starts working on a project upon receipt of a down payment. Unless otherwise specified in writing, the client is charged the remaining balance upon completion of the project. Websites are made live upon completion of the project, when all payments are cleared. If applicable, hosting and domain name registration fees must be paid every month.</p>
 
-    <p>3. Delays in project completion may occur when:</p>
+        <p className="mt-3">2. Additional services come at additional fees, on a fee for service basis or on an hourly rate. If clients require additional services, the correspondent fee is added to their invoice and they are charged accordingly.</p>
 
-    <ul className="list--unstyled">
-      <li>There is a delay in any payments</li>
+        <p className="mt-3">3. Delays in project completion may occwur when:</p>
 
-      <li>The client requests further changes in the design, structure, or content of the project</li>
+        <ul className="list--unstyled mt-3">
+          <li>&mdash;There is a delay in any payments</li>
 
-      <li>The client purchases an additional service related to the project</li>
+          <li>&mdash;The client requests further changes in the design, structure, or content of the project</li>
 
-      <li>There is a delay in the receipt of any materials form the client</li>
+          <li>&mdash;The client purchases an additional service related to the project</li>
 
-      <li>Time frame estimates are approximate only and are subject to change at any time at Devero Studio's discretion. At any time, and at its whole discretion, Devero Studio's may decide to put a project on hold.</li>
-    </ul>
+          <li>&mdash;There is a delay in the receipt of any materials form the client</li>
 
-    <p>4. All of our services are 100% satisfaction guaranteed. We don't stop working until our clients are completely satisfied with their solution. That is our commitment. However, Devero Studio will under no circumstances be held responsible to satisfy unreasonable expectations or request Devero Studio‘s discretion to decide whether or not a request or an expectation is reasonable or lies within the scope of offer of services to its client. As an example, if the client purchases a certain service, but in order to be fully satisfied, Devero Studio judges that an additional service is necessary, then it is no longer Devero Studio's responsibility or commitment to satisfy the client. In other words, Devero Studio guarantees 100% satisfaction within the framework of its original agreement with the client, according to Devero Studio's understanding.</p>
+          <li>&mdash;Time frame estimates are approximate only and are subject to change at any time at Devero Studio's discretion. At any time, and at its whole discretion, Devero Studio's may decide to put a project on hold.</li>
+        </ul>
 
-    <p>5. Services or part of services (i.e. elements of a project) that have been approved by the client are considered as services with which the client is 100% satisfied. Any further work on these services (elements of the project) will be invoiced accordingly.</p>
+        <p className="mt-3">4. All of our services are 100% satisfaction guaranteed. We don't stop working until our clients are completely satisfied with their solution. That is our commitment. However, Devero Studio will under no circumstances be held responsible to satisfy unreasonable expectations or request Devero Studio‘s discretion to decide whether or not a request or an expectation is reasonable or lies within the scope of offer of services to its client. As an example, if the client purchases a certain service, but in order to be fully satisfied, Devero Studio judges that an additional service is necessary, then it is no longer Devero Studio's responsibility or commitment to satisfy the client. In other words, Devero Studio guarantees 100% satisfaction within the framework of its original agreement with the client, according to Devero Studio's understanding.</p>
 
-    <p>6. Unless clearly specified otherwise, all project statuses are set as “completed” after a 3 month period has passed (calculated from the first payment date).This means no more work will be done on that project and no service will be provided. This doesn't apply to hosting and domain name registration services, which are provided independently of the project status, and only according to the client's payments.</p>
+        <p className="mt-3">5. Services or part of services (i.e. elements of a project) that have been approved by the client are considered as services with which the client is 100% satisfied. Any further work on these services (elements of the project) will be invoiced accordingly.</p>
 
-    <p>7. If client is not satisfied with Devero Studio's Services, client can ask for a refund on any unused monies. Devero Studio will be happy to exercise a refund, after doing it on review.</p>
+        <p className="mt-3">6. Unless clearly specified otherwise, all project statuses are set as “completed” after a 3 month period has passed (calculated from the first payment date).This means no more work will be done on that project and no service will be provided. This doesn't apply to hosting and domain name registration services, which are provided independently of the project status, and only according to the client's payments.</p>
+
+        <p className="mt-3">7. If client is not satisfied with Devero Studio's Services, client can ask for a refund on any unused monies. Devero Studio will be happy to exercise a refund, after doing it on review.</p>
+      </div>
+    </div>
   </div>
+
+  <Footer />
 </div>)
