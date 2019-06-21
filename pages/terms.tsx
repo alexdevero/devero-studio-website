98bed877cd27bc9a0daa5475a9d0c8ac63b9cc11
Alex Devero <deveroalex@gmail.com>
2019-06-21 12:49:37 +0200

    Add Terms and conditions page

---
+++ pages/terms.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+
+export default () => (<div>
+  <Head>
+    <title>Terms and Conditions | Devero Studio</title>
+  </Head>
+
+  <div>
+    <h1>Terms and Conditions</h1>
+
+    <p>1. Devero Studio starts working on a project upon receipt of a down payment. Unless otherwise specified in writing, the client is charged the remaining balance upon completion of the project. Websites are made live upon completion of the project, when all payments are cleared. If applicable, hosting and domain name registration fees must be paid every month.</p>
+
+    <p>2. Additional services come at additional fees, on a fee for service basis or on an hourly rate. If clients require additional services, the correspondent fee is added to their invoice and they are charged accordingly.</p>
+
+    <p>3. Delays in project completion may occur when:</p>
+
+    <ul className="list--unstyled">
+      <li>There is a delay in any payments</li>
+
+      <li>The client requests further changes in the design, structure, or content of the project</li>
+
+      <li>The client purchases an additional service related to the project</li>
+
+      <li>There is a delay in the receipt of any materials form the client</li>
+
+      <li>Time frame estimates are approximate only and are subject to change at any time at Devero Studio's discretion. At any time, and at its whole discretion, Devero Studio's may decide to put a project on hold.</li>
+    </ul>
+
+    <p>4. All of our services are 100% satisfaction guaranteed. We don't stop working until our clients are completely satisfied with their solution. That is our commitment. However, Devero Studio will under no circumstances be held responsible to satisfy unreasonable expectations or request Devero Studio‘s discretion to decide whether or not a request or an expectation is reasonable or lies within the scope of offer of services to its client. As an example, if the client purchases a certain service, but in order to be fully satisfied, Devero Studio judges that an additional service is necessary, then it is no longer Devero Studio's responsibility or commitment to satisfy the client. In other words, Devero Studio guarantees 100% satisfaction within the framework of its original agreement with the client, according to Devero Studio's understanding.</p>
+
+    <p>5. Services or part of services (i.e. elements of a project) that have been approved by the client are considered as services with which the client is 100% satisfied. Any further work on these services (elements of the project) will be invoiced accordingly.</p>
+
+    <p>6. Unless clearly specified otherwise, all project statuses are set as “completed” after a 3 month period has passed (calculated from the first payment date).This means no more work will be done on that project and no service will be provided. This doesn't apply to hosting and domain name registration services, which are provided independently of the project status, and only according to the client's payments.</p>
+
+    <p>7. If client is not satisfied with Devero Studio's Services, client can ask for a refund on any unused monies. Devero Studio will be happy to exercise a refund, after doing it on review.</p>
+  </div>
+</div>)
