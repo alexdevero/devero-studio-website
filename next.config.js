4af2abaefd88c82e57607c18a0369889858060b1
Alex Devero <deveroalex@gmail.com>
2019-06-22 10:40:30 +0200

    Add next-css plugin

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const withOptimizedImages = require('next-optimized-images')
 const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
+const withCSS = require('@zeit/next-css')
 
-module.exports = withTypescript(withOptimizedImages(
+module.exports = withTypescript(withOptimizedImages(withCSS(
   withSass({
     webpack(config, options) {
       return config
     }
-  }))
+  })))
 )
