f7213383daae125d75fb3a27ea588a510aa500f5
Alex Devero <deveroalex@gmail.com>
2019-07-06 11:34:41 +0200

    Add author photos to testimonials

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -8,7 +8,7 @@ export const Testimonials = () => {
     sectionTestimonials!.classList.add('fade-in-up')
   }
 
-  return(
+  return (
     <Waypoint onEnter={handleWaypointEnter} topOffset="-40%">
       <section className="animated" ref={el => sectionTestimonials = el}>
         <div className="container pt-5 pb-5">
@@ -20,21 +20,33 @@ export const Testimonials = () => {
             <div className="col-md-4 col-lg-4">
               <blockquote>
                 <p className="blockquote__text">&ldquo;Team at Devero Studio is great to work with. Very professional, responsive and delivered outstanding work just like I had imagined as the outcome. Will definitely hire them again.&rdquo;</p>
-                <footer className="blockquote__footer">&mdash;Jeremy Fisher, CEO of RocketSpur</footer>
+                <footer className="blockquote__footer">
+                  <img className="blockquote__avatar" src={require('./../static/images/testimonials-photos/photo-jeremy-fisher.jpg')} />
+
+                  <div className="blockquote__autor">Jeremy Fisher<br/>CEO of RocketSpur</div>
+                </footer>
               </blockquote>
             </div>
 
             <div className="col-md-4 col-lg-4">
               <blockquote>
                 <p className="blockquote__text">&ldquo;Devero Studio proved to be a very good choice. Team at Devero Studio did a great job re-designing our website. It exceeded my expectations from design to development.&rdquo;</p>
-                <footer className="blockquote__footer">&mdash;Mike Hurley, CTO at Clava</footer>
+                <footer className="blockquote__footer">
+                  <img className="blockquote__avatar" src={require('./../static/images/testimonials-photos/photo-mike-hurley.jpg')} />
+
+                  <div className="blockquote__autor">Mike Hurley<br />CTO at Clava</div>
+                </footer>
               </blockquote>
             </div>
 
             <div className="col-md-4 col-lg-4">
               <blockquote>
                 <p className="blockquote__text">&ldquo;Team at Devero Studio has been a tremendous help. What would have taken other agencies 6 months only took them 1.5 months.&rdquo;</p>
-                <footer className="blockquote__footer">&mdash;Terrence Jackson, CEO of Ringio</footer>
+                <footer className="blockquote__footer">
+                  <img className="blockquote__avatar" src={require('./../static/images/testimonials-photos/photo-terrence-jackson.jpg')} />
+
+                  <div className="blockquote__autor">Terrence Jackson<br />CEO of Ringio</div>
+                </footer>
               </blockquote>
             </div>
           </div>
@@ -43,21 +55,33 @@ export const Testimonials = () => {
             <div className="col-md-4 col-lg-4">
               <blockquote>
                 <p className="blockquote__text">&ldquo;Great work and done in time! Very important for time-sensitive projects.&rdquo;</p>
-                <footer className="blockquote__footer">&mdash;Terri Morris, VP of product at iServe</footer>
+                <footer className="blockquote__footer">
+                  <img className="blockquote__avatar" src={require('./../static/images/testimonials-photos/photo-terri-morris.jpg')} />
+
+                  <div className="blockquote__autor">Terri Morris<br />VP of product at iServe</div>
+                </footer>
               </blockquote>
             </div>
 
             <div className="col-md-4 col-lg-4">
               <blockquote>
                 <p className="blockquote__text">&ldquo;People at Devero Studio was a pleasure to work with. They compiled my vision into a true masterpiece, that I am proud to display.&rdquo;</p>
-                <footer className="blockquote__footer">&mdash;Joshua Harvey, CTO of Volocity</footer>
+                <footer className="blockquote__footer">
+                  <img className="blockquote__avatar" src={require('./../static/images/testimonials-photos/photo-joshua-harvey.jpg')} />
+
+                  <div className="blockquote__autor">Joshua Harvey<br />CTO of Volocity</div>
+                </footer>
               </blockquote>
             </div>
 
             <div className="col-md-4 col-lg-4">
               <blockquote>
                 <p className="blockquote__text">&ldquo;Devero Studio is professional, very reliable, flexible, and delivers on time. They are able to put your ideas into practice often better than your own conception, incredibly fortunate to work with this agency.&rdquo;</p>
-                <footer className="blockquote__footer">&mdash;Ryan Ferguson, Director of Engineering at Homesnap</footer>
+                <footer className="blockquote__footer">
+                  <img className="blockquote__avatar" src={require('./../static/images/testimonials-photos/photo-ryan-ferguson.jpg')} />
+
+                  <div className="blockquote__autor">Ryan Ferguson<br/>Director of Engineering at Homesnap</div>
+                </footer>
               </blockquote>
             </div>
           </div>
