a4ea71ccb41e961fe9f53fd69b9a4b0e7aaf1308
Alex Devero <deveroalex@gmail.com>
2019-07-11 09:01:24 +0200

    Implement next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
+const withPlugins = require('next-compose-plugins')
 const withOptimizedImages = require('next-optimized-images')
 const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withTypescript(withOptimizedImages(withCSS(
-  withSass({
-    webpack(config, options) {
-      return config
-    }
-  })))
-)
+module.exports = withPlugins([
+  withCSS,
+  withSass,
+  withOptimizedImages,
+  withTypescript
+])
+
+// module.exports = withTypescript(withOptimizedImages(withCSS(
+//   withSass({
+//     webpack(config, options) {
+//       return config
+//     }
+//   })))
+// )
