a068e3a6185beee2968252dea64a3f9fa79fd962
Alex Devero <deveroalex@gmail.com>
2019-07-11 09:30:48 +0200

    Add config for next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,29 @@
+export default {
+  title: 'Devero Studio - Digital agency creating web and mobile products',
+  description: 'Devero Studio is the creative team helping clients worldwide with web and mobile design and development. We design and build top-class websites and apps for those who want the best.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://www.studio.devero.io/',
+    site_name: 'Devero Studio',
+    images: [
+      {
+        url: 'https://www.studio.devero.io/og-image-01.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Og Image Alt'
+      },
+      {
+        url: 'https://www.studio.devero.io/og-image-02.jpg',
+        width: 900,
+        height: 800,
+        alt: 'Og Image Alt Second'
+      }
+    ]
+  },
+  twitter: {
+    handle: '@devero',
+    site: '@devero',
+    cardType: 'summary_large_image'
+  }
+}
