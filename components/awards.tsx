b85a4258d1694f669e2335c0b8078f36c769de31
Alex Devero <deveroalex@gmail.com>
2019-07-18 10:25:50 +0200

    Update style of awards logos

--- components/awards.tsx
+++ components/awards.tsx
@@ -17,33 +17,33 @@ export const Awards = () => {
 
         <div className="row text--center">
           <div className="col-sm-6 col-md-3 logo--csswinner">
-            <img className="icon--medium" src={require('./../static/images/logos-awards/logo-csswinner.svg')} alt="CSSWinner logo"/>
+            <img className="icon--medium icon--faded" src={require('./../static/images/logos-awards/logo-csswinner.svg')} alt="CSSWinner logo"/>
           </div>
 
           <div className="col-sm-6 col-md-3">
-            <img className="icon--medium" src={require('./../static/images/logos-awards/logo-cssawards.svg')} alt="CSSAwards logo"/>
+            <img className="icon--medium icon--faded" src={require('./../static/images/logos-awards/logo-cssawards.svg')} alt="CSSAwards logo"/>
           </div>
 
           <div className="col-sm-6 col-md-3">
-            <img className="icon--medium" src={require('./../static/images/logos-awards/logo-awwwards.svg')} alt="awwwards logo"/>
+            <img className="icon--medium icon--faded" src={require('./../static/images/logos-awards/logo-awwwards.svg')} alt="awwwards logo"/>
           </div>
 
           <div className="col-sm-6 col-md-3">
-            <img className="icon--medium" style={{ width: 80 }} src={require('./../static/images/logos-awards/logo-webby-awards.svg')} alt="Webby Awards logo"/>
+            <img className="icon--medium icon--faded" style={{ width: 80 }} src={require('./../static/images/logos-awards/logo-webby-awards.svg')} alt="Webby Awards logo"/>
           </div>
         </div>
 
         <div className="row text--center">
           <div className="col-sm-6 col-md-4 logo--csswinner">
-            <img className="icon--medium" style={{ width: 80 }} src={require('./../static/images/logos-awards/logo-net.svg')} alt="Net Magazine logo"/>
+            <img className="icon--medium icon--faded" style={{ width: 80 }} src={require('./../static/images/logos-awards/logo-net.svg')} alt="Net Magazine logo"/>
           </div>
 
           <div className="col-sm-6 col-md-4 logo--smasing-magazine">
-            <img className="icon--medium" style={{ position: 'relative', top: 12 }} src={require('./../static/images/logos-awards/logo-smashing-magazine.svg')} alt="Smashing Magazine logo"/>
+            <img className="icon--medium icon--faded" style={{ position: 'relative', top: 12 }} src={require('./../static/images/logos-awards/logo-smashing-magazine.svg')} alt="Smashing Magazine logo"/>
           </div>
 
           <div className="col-sm-6 col-md-4 logo--web-designer">
-            <img className="icon--medium" style={{ width: 60 }} src={require('./../static/images/logos-awards/logo-web-designer.svg')} alt="Web Designer magazine logo"/>
+            <img className="icon--medium icon--faded" style={{ width: 60 }} src={require('./../static/images/logos-awards/logo-web-designer.svg')} alt="Web Designer magazine logo"/>
           </div>
         </div>
       </div>
