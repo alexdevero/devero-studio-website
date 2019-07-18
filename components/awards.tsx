ed7c86ccde9576fdfeb554cc7647811e7c2224be
Alex Devero <deveroalex@gmail.com>
2019-07-18 11:53:55 +0200

    Import award logos as svg components

--- components/awards.tsx
+++ components/awards.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Waypoint } from 'react-waypoint'
+import ReactSVG from 'react-svg'
 
 export const Awards = () => {
   let sectionAwards: HTMLDivElement | null
@@ -13,37 +14,84 @@ export const Awards = () => {
       <div className="container animated" ref={el => sectionAwards = el}>
         <h1 className="h4 heading--small text--center">Featured in</h1>
 
-        <div className="divider divider--red divider--center"></div>
+        <div className="divider divider--red divider--center" />
 
-        <div className="row text--center">
-          <div className="col-sm-6 col-md-3 logo--csswinner">
-            <img className="icon--medium icon--faded" src={require('./../static/images/logos-awards/logo-csswinner.svg')} alt="CSSWinner logo"/>
+        <div className="awards-row row text--center">
+          <div className="col-sm-6 col-md-3 d-flex align-items-center justify-content-center">
+            <ReactSVG
+              beforeInjection={svg => {
+                svg.classList.add('logo-csswinner')
+                svg.setAttribute('style', 'width: 110px')
+              }}
+              src='./../static/images/logos-awards/logo-csswinner.svg'
+            />
           </div>
 
-          <div className="col-sm-6 col-md-3">
-            <img className="icon--medium icon--faded" src={require('./../static/images/logos-awards/logo-cssawards.svg')} alt="CSSAwards logo"/>
+          <div className="col-sm-6 col-md-3 d-flex align-items-center justify-content-center">
+            <ReactSVG
+              beforeInjection={svg => {
+                svg.classList.add('logo-cssawards')
+                svg.setAttribute('style', 'width: 110px')
+              }}
+              src='./../static/images/logos-awards/logo-cssawards.svg'
+            />
           </div>
 
-          <div className="col-sm-6 col-md-3">
-            <img className="icon--medium icon--faded" src={require('./../static/images/logos-awards/logo-awwwards.svg')} alt="awwwards logo"/>
+          <div className="col-sm-6 col-md-3 d-flex align-items-center justify-content-center">
+            <ReactSVG
+              beforeInjection={svg => {
+                svg.classList.add('logo-awwwards')
+                svg.setAttribute('style', 'width: 110px')
+              }}
+              src='./../static/images/logos-awards/logo-awwwards.svg'
+            />
           </div>
 
-          <div className="col-sm-6 col-md-3">
-            <img className="icon--medium icon--faded" style={{ width: 80 }} src={require('./../static/images/logos-awards/logo-webby-awards.svg')} alt="Webby Awards logo"/>
+          <div className="col-sm-6 col-md-3 d-flex align-items-center justify-content-center">
+            <ReactSVG
+              beforeInjection={svg => {
+                svg.classList.add('logo-webby-awards')
+                svg.setAttribute('style', 'width: 80px')
+              }}
+              src='./../static/images/logos-awards/logo-webby-awards.svg'
+            />
           </div>
         </div>
 
         <div className="row text--center">
-          <div className="col-sm-6 col-md-4 logo--csswinner">
-            <img className="icon--medium icon--faded" style={{ width: 80 }} src={require('./../static/images/logos-awards/logo-net.svg')} alt="Net Magazine logo"/>
+          <div className="col-sm-6 col-md-4 d-flex align-items-center justify-content-center">
+            <ReactSVG
+              beforeInjection={svg => {
+                svg.classList.add('logo-csswinner')
+                svg.setAttribute('style', 'width: 80px')
+              }}
+              src='./../static/images/logos-awards/logo-net.svg'
+            />
           </div>
 
-          <div className="col-sm-6 col-md-4 logo--smasing-magazine">
-            <img className="icon--medium icon--faded" style={{ position: 'relative', top: 12 }} src={require('./../static/images/logos-awards/logo-smashing-magazine.svg')} alt="Smashing Magazine logo"/>
+          <div className="col-sm-6 col-md-4 d-flex align-items-center justify-content-center">
+            <ReactSVG
+              beforeInjection={svg => {
+                svg.classList.add('logo-smashing-magazine')
+                svg.setAttribute('style', 'width: 170px; filter: brightness(0);')
+              }}
+              src='./../static/images/logos-awards/logo-smashing-magazine.svg'
+            />
           </div>
 
-          <div className="col-sm-6 col-md-4 logo--web-designer">
-            <img className="icon--medium icon--faded" style={{ width: 60 }} src={require('./../static/images/logos-awards/logo-web-designer.svg')} alt="Web Designer magazine logo"/>
+          <div className="col-sm-6 col-md-4 d-flex align-items-center justify-content-center">
+            <ReactSVG
+              beforeInjection={svg => {
+                svg.classList.add('logo-web-designer')
+                svg.setAttribute('style', 'width: 80px')
+              }}
+              src='./../static/images/logos-awards/logo-web-designer.svg'
+            />
+            {/* <img
+              alt="Web Designer magazine logo"
+              className="icon--medium icon--faded"
+              src={require('./../static/images/logos-awards/logo-web-designer.svg')} style={{ width: 60 }}
+            /> */}
           </div>
         </div>
       </div>
