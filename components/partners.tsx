2ba79720f235b8c33bdd433fa6ca4aa20fc71418
Alex Devero <deveroalex@gmail.com>
2019-07-20 09:22:38 +0200

    Prepare component for Partners

--- components/partners.tsx
+++ components/partners.tsx
@@ -6,7 +6,7 @@ export const Partners = () => {
       <div className="container pt-5 pb-5">
         <h1 className="h3 heading--small text--center">Who We Work With</h1>
 
-        <div className="divider divider--black divider--center"></div>
+        <div className="divider divider--red divider--center"></div>
 
         <div className="row">
           <div className="col-md-10 col-lg-8"></div>
