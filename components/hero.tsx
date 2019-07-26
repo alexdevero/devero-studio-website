38a7c78f54046bea1029c0f43f1d691dea2410b2
Alex Devero <deveroalex@gmail.com>
2019-07-26 13:10:16 +0200

    Add react-spring blob to hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react'
+import { useTrail, animated } from 'react-spring'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
+const fast = { tension: 1200, friction: 40 }
+const slow = { mass: 10, tension: 200, friction: 50 }
+const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
+
 export const Hero = () => {
-  return(
+  const [trail, set] = useTrail(3, () => ({ xy: [0, 0], config: i => (i === 0 ? fast : slow) }))
+
+  return (
     <div className="hero">
       <div className="container">
         <h1 className="hero__heading">DEVERO STUDIO</h1>
@@ -22,6 +29,19 @@ export const Hero = () => {
         <h3 className="hero__text">We design and build top-class websites and apps for those who want the best.</h3>
 
         <AnchorLink className="hero__link" href="#learnMore">&darr; Learn more</AnchorLink>
+      </div>
+
+      <svg style={{ position: 'absolute', width: 0, height: 0 }}>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
+          <feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" />
+        </filter>
+      </svg>
+
+      <div className="hooks-main" onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}>
+        {trail.map((props, index) => (
+          <animated.div key={index} style={{ transform: props.xy.interpolate(trans) }} />
+        ))}
       </div>
     </div>
   )
