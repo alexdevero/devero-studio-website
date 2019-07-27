c5a34c737864d68ee0d890ae2783ea4a2b46948f
Alex Devero <deveroalex@gmail.com>
2019-07-27 05:43:33 +0200

    Fix divider

--- components/faq.tsx
+++ components/faq.tsx
@@ -6,7 +6,7 @@ export const Faq = () => {
       <div className="container pt-5 pb-5">
         <h1 className="h3 heading--small text--center">F.A.Q</h1>
 
-        <div className="divider divider--black divider--center"></div>
+        <div className="divider divider--red divider--center"></div>
       </div>
     </section>
   )
