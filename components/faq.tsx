ec5327cb48a1941c7fde72d1fe9fe9c47ae98821
Alex Devero <deveroalex@gmail.com>
2019-08-09 10:32:31 +0200

    Add content for faq

--- components/faq.tsx
+++ components/faq.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react'
 
 export const Faq = () => {
-  return(
+  return (
     <section>
       <div className="container pt-5 pb-5">
         <h1 className="h3 heading--small text--center">F.A.Q</h1>
 
-        <div className="divider divider--red divider--center"></div>
+        <div className="divider divider--red divider--center" />
+
+        <h2 className="h3">How Much Does a Website Homepage Cost?</h2>
+
+        <p>A homepage (or landing page) project is a type of web design job that is commonly the second step companies take when looking to enhance their brand. Depending on the complexity, you can expect to pay anything from $500 for a simple teaser to $5,000 for a full-featured landing page introducing your product (think Basecamp&apos;s homepage). The average price is around $1,900.</p>
+
+        <h2 className="h3 mt-5">How Much Does a Full Static Website Cost?</h2>
+
+        <p>Website projects are the most in-demand type of contract. They&apos;re also one of the hardest jobs to price. Asking how much one should cost, is like asking an architect how much a house will cost. It all depends on specifics, so usually a straight answer isn&apos;t easy. Still, we can say that the usual range is between $2,500 and $5,000. The average cost for a predictably great result is around $3,500. This assumes a basic website, that has a homepage, a basic page template, and 1 or 2 additional layouts, no backend. Beyond that you&apos;re looking at a lot more, so keep this in mind.</p>
+
+        <h2 className="h3 mt-5">How Much Does a Web App Design Cost?</h2>
+
+        <p>Unlike a static site, a web app is constantly evolving. That makes requirements harder to pin down, and planning for unexpected new features a must. Also, designing a web app requires a thorough UI/UX: to make it look good AND work great. All of this explains why this is one of the most expensive categories. For starters, we recommend budget of at least $4,000, but you&apos;re probably looking at $7,000+. It is important to mention that $15,000 or even $20,000 is nothing out of the ordinary so plan accordingly.</p>
+
+        <h2 className="h3 mt-5">How Much Does a Mobile App Design Cost?</h2>
+
+        <p>Mobile app design jobs and contracts are a more specialty type of design job. For example, mobile apps frequently require a closer attention to aesthetics, UX journey maps, and visual details than websites. These factors mean that even with less pages or less content, mobile apps are not cheaper than other project. Due to this, the average cost is around $5,000.</p>
       </div>
     </section>
   )
