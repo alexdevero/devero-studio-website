85b26ada2ef8cd8e7214778a2cf4b9247e2360fc
Alex Devero <deveroalex@gmail.com>
2019-08-10 16:06:09 +0200

    Extract react-spring blob to component

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react'
-// import { useTrail, animated } from 'react-spring'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import Lottie from 'lottie-react-web'
 
 import animation from './../static/lottie-animation.json'
 
-// const fast = { tension: 1200, friction: 40 }
-// const slow = { mass: 10, tension: 200, friction: 50 }
-// const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
-
 export const Hero = () => {
-  // const [trail, set] = useTrail(3, () => ({ xy: [0, 0], config: i => (i === 0 ? fast : slow) }))
-
   return (
     <div className="hero">
       <div className="container">
@@ -37,19 +30,6 @@ export const Hero = () => {
           animationData: animation
         }}
       />
-
-      {/* <svg style={{ position: 'absolute', width: 0, height: 0 }}>
-        <filter id="goo">
-          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
-          <feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" />
-        </filter>
-      </svg>
-
-      <div className="hooks-main" onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}>
-        {trail.map((props, index) => (
-          <animated.div key={index} style={{ transform: props.xy.interpolate(trans) }} />
-        ))}
-      </div> */}
 
       <AnchorLink className="hero__link" href="#intro"><span className="fas fa-long-arrow-alt-down" /> Learn more</AnchorLink>
     </div>
