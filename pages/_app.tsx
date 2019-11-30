7816975b2b37ddc300e7fce18939ba0a0a59e459
Alex Devero <deveroalex@gmail.com>
2019-11-30 11:31:02 +0100

    Simplify app compoent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,16 @@
 import * as React from 'react'
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import Head from 'next/head'
 import NextSeo from 'next-seo'
 
 import SEOConfig from './../next-seo.config'
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render() {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
+      <>
         <NextSeo config={SEOConfig} />
 
         <Head>
@@ -50,7 +40,7 @@ class MyApp extends App {
         </Head>
 
         <Component {...pageProps} />
-      </Container>
+      </>
     )
   }
 }
