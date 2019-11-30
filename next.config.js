6251a7badc87eda303c5708214299e4e8cde16fc
Alex Devero <deveroalex@gmail.com>
2019-11-30 10:23:40 +0100

    Disaable next typescript plugin

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const withPlugins = require('next-compose-plugins')
 const withOptimizedImages = require('next-optimized-images')
 const withSass = require('@zeit/next-sass')
-const withTypescript = require('@zeit/next-typescript')
+// const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
 
 module.exports = withPlugins([
   withCSS,
   withSass,
   withOptimizedImages,
-  withTypescript
+  // withTypescript
 ])
 
 // module.exports = withTypescript(withOptimizedImages(withCSS(
