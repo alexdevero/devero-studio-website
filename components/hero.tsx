1a2f642211e3a8175a66bb755a055f9af878316b
Alex Devero <deveroalex@gmail.com>
2019-11-30 14:04:34 +0100

    Restyle hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,28 +8,34 @@ export const Hero = () => {
   return (
     <div className="hero">
       <div className="container">
-        <h1 className="hero__heading">DEVERO STUDIO</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="hero__heading">Crafting beautiful websites & apps</h1>
 
-        {/* <div className="glitch-wrapper d-md-none">
-          <h1 className="hero__title">
-            <span className="glitch" data-text="Delivering">Delivering</span>
-            <span className="glitch" data-text="digital">digital</span>
-            <span className="glitch" data-text="perfection">perfection</span>
-          </h1>
-        </div> */}
+            {/* <div className="glitch-wrapper d-md-none">
+              <h1 className="hero__title">
+                <span className="glitch" data-text="Delivering">Delivering</span>
+                <span className="glitch" data-text="digital">digital</span>
+                <span className="glitch" data-text="perfection">perfection</span>
+              </h1>
+            </div> */}
 
-        {/* <div className="glitch-wrapper d-none d-md-block">
-          <h1 className="hero__title glitch" data-text="Delivering digital perfection">Delivering digital perfection</h1>
-        </div> */}
+            {/* <div className="glitch-wrapper d-none d-md-block">
+              <h1 className="hero__title glitch" data-text="Delivering digital perfection">Delivering digital perfection</h1>
+            </div> */}
 
-        <h3 className="hero__text">We design and build top-class websites and apps for those who want the best.</h3>
-      </div>
+            <h3 className="hero__text">Devero Studio is a digital agency specializing in mobile and web design and development.</h3>
+          </div>
 
-      <Lottie
-        options={{
-          animationData: animation
-        }}
-      />
+          <div className="col-md-6">
+            <Lottie
+              options={{
+                animationData: animation
+              }}
+            />
+          </div>
+          </div>
+        </div>
 
       <AnchorLink className="hero__link" href="#intro"><span className="fas fa-long-arrow-alt-down" /> Learn more</AnchorLink>
     </div>
