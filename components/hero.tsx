a2a1a8f957d4de8c0ff789cbccbc79b71c8ab49f
Alex Devero <deveroalex@gmail.com>
2019-12-23 13:47:25 +0100

    Update hero subheading

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,7 +24,7 @@ export const Hero = () => {
               <h1 className="hero__title glitch" data-text="Delivering digital perfection">Delivering digital perfection</h1>
             </div> */}
 
-            <h3 className="hero__text">Devero Studio is a digital agency specializing in mobile and web design and development.</h3>
+            <h3 className="hero__text">Devero Studio is a digital agency specializing in web and mobile design and development.</h3>
           </div>
 
           <div className="col-md-6">
