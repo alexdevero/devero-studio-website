605502bde01c2620f1f823119c27c22ff7ae5f37
Alex Devero <deveroalex@gmail.com>
2019-12-24 16:12:56 +0100

    Fix external footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,55 +11,55 @@ export const Footer = () => {
 
             <ul className="list--inline list--unstyled mt-3">
               <li className="list--inline-item">
-                <a href="https://twitter.com/alexdevero" target="_blank">
+                <a href="https://twitter.com/alexdevero" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-twitter"></span>
                 </a>
               </li>
 
               <li className="list--inline-item">
-                <a href="https://github.com/alexdevero" target="_blank">
+                <a href="https://github.com/alexdevero" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-github"></span>
                 </a>
               </li>
 
               {/* <li className="list--inline-item">
-                <a href="https://codesandbox.io/u/alexdevero" target="_blank">
+                <a href="https://codesandbox.io/u/alexdevero" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-sandbox"></span>
                 </a>
               </li> */}
 
               <li className="list--inline-item">
-                <a href="https://codepen.io/alexdevero/" target="_blank">
+                <a href="https://codepen.io/alexdevero/" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-codepen"></span>
                 </a>
               </li>
 
               <li className="list--inline-item">
-                <a href="https://stackoverflow.com/story/alexdevero" target="_blank">
+                <a href="https://stackoverflow.com/story/alexdevero" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-stack-overflow"></span>
                 </a>
               </li>
 
               <li className="list--inline-item">
-                <a href="https://dribbble.com/alexdevero" target="_blank">
+                <a href="https://dribbble.com/alexdevero" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-dribbble"></span>
                 </a>
               </li>
 
               {/* <li className="list--inline-item">
-                <a href="https://www.behance.net/alexdevero" target="_blank">
+                <a href="https://www.behance.net/alexdevero" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-behance"></span>
                 </a>
               </li> */}
 
               {/* <li className="list--inline-item">
-                <a href="https://www.facebook.com/deveroalex" target="_blank">
+                <a href="https://www.facebook.com/deveroalex" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-facebook"></span>
                 </a>
               </li> */}
 
               <li className="list--inline-item">
-                <a href="https://www.instagram.com/alexdevero/" target="_blank">
+                <a href="https://www.instagram.com/alexdevero/" target="_blank" rel="noopener noreferrer">
                   <span className="fab fa-instagram"></span>
                 </a>
               </li>
