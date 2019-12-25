ca97c360a4e66d0793dcdba8d01d1efceb73414d
Alex Devero <deveroalex@gmail.com>
2019-12-25 11:26:42 +0100

    Add header navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,40 @@
 import * as React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-export const Header = () => {}
+interface HeaderInterface {
+  handleQuoteClick: () => void;
+}
+
+export const Header = (props: HeaderInterface) => (
+  <header className="header">
+    <div className="container">
+      <nav>
+        <ul className="list--inline list--unstyled text--right">
+          <li className="list--inline-item">
+            <AnchorLink href="#works">Works</AnchorLink>
+          </li>
+
+          <li className="list--inline-item">
+            <AnchorLink href="#whatWeDo">Services</AnchorLink>
+          </li>
+
+          <li className="list--inline-item">
+            <AnchorLink href="#ourProcess">Process</AnchorLink>
+          </li>
+
+          <li className="list--inline-item">
+            <a href="https://blog.alexdevero.com" target="_blank" rel="noopener noreferrer">Blog</a>
+          </li>
+
+          <li className="list--inline-item">
+            <AnchorLink href="#contact">Hire us</AnchorLink>
+          </li>
+
+          <li className="list--inline-item quote-btn-wrapper">
+            <button className="btn btn--black-red" onClick={props.handleQuoteClick} data-text="Get quote">Get quote</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+)
