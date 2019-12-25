c257ad7d8529cf9a3f3cc33464ed70130f444824
Alex Devero <deveroalex@gmail.com>
2019-12-25 15:48:54 +0100

    Restyle navigation for mobile

--- components/header.tsx
+++ components/header.tsx
@@ -5,36 +5,50 @@ interface HeaderInterface {
   handleQuoteClick: () => void;
 }
 
-export const Header = (props: HeaderInterface) => (
-  <header className="header">
-    <div className="container">
-      <nav>
-        <ul className="list--inline list--unstyled text--right">
-          <li className="list--inline-item">
-            <AnchorLink href="#works">Works</AnchorLink>
-          </li>
+export const Header = (props: HeaderInterface) => {
+  const [isNavVisible, setIsNavVisible] = React.useState(false)
 
-          <li className="list--inline-item">
-            <AnchorLink href="#whatWeDo">Services</AnchorLink>
-          </li>
+  return (
+    <header className="header">
+      <div className="container">
+        <button className="header-mobile-btn" onClick={() => setIsNavVisible(!isNavVisible)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
-          <li className="list--inline-item">
-            <AnchorLink href="#ourProcess">Process</AnchorLink>
-          </li>
+        <nav className={isNavVisible ? 'nav nav-visible' : 'nav'}>
+          <ul className="list--inline list--unstyled text--right">
+            <li className="list--inline-item">
+              <button className="quote-modal__button header-mobile-btn--close" onClick={() => setIsNavVisible(!isNavVisible)} />
+            </li>
 
-          <li className="list--inline-item">
-            <a href="https://blog.alexdevero.com" target="_blank" rel="noopener noreferrer">Blog</a>
-          </li>
+            <li className="list--inline-item">
+              <AnchorLink href="#works">Works</AnchorLink>
+            </li>
 
-          <li className="list--inline-item">
-            <AnchorLink href="#contact">Hire us</AnchorLink>
-          </li>
+            <li className="list--inline-item">
+              <AnchorLink href="#whatWeDo">Services</AnchorLink>
+            </li>
 
-          <li className="list--inline-item quote-btn-wrapper">
-            <button className="btn btn--black-red" onClick={props.handleQuoteClick} data-text="Get quote">Get quote</button>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-)
+            <li className="list--inline-item">
+              <AnchorLink href="#ourProcess">Process</AnchorLink>
+            </li>
+
+            <li className="list--inline-item">
+              <a href="https://blog.alexdevero.com" target="_blank" rel="noopener noreferrer">Blog</a>
+            </li>
+
+            <li className="list--inline-item">
+              <AnchorLink href="#contact">Hire us</AnchorLink>
+            </li>
+
+            <li className="list--inline-item quote-btn-wrapper">
+              <button className="btn btn--black-red" onClick={props.handleQuoteClick} data-text="Get quote">Get quote</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
